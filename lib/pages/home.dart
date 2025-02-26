28c0d92dfbe73be9d64568f2915988a21560f370
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2025-02-27 00:41:38 +0300

    search bar added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,16 +7,77 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Breakfast',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          searchField(),
+        ],
+      ),
+    );
+  }
+
+  Container searchField() {
+    return Container(
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            color: Color(0xff1D1617).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0)
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.all(15),
+            hintText: 'Search Pancake',
+            hintStyle: TextStyle(
+              color: Color(0xffD0DADA),
+              fontSize: 14,
+            ),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset('assets/icons/Search.svg'),
+            ),
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: SvgPicture.asset('assets/icons/Filter.svg'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none)),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () => {},
+        child: Container(
           margin: EdgeInsets.all(10),
           alignment: Alignment.center,
           decoration: BoxDecoration(
@@ -28,8 +89,11 @@ class HomePage extends StatelessWidget {
             width: 20,
           ),
         ),
-        actions: [
-          Container(
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () => {},
+          child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
             width: 37,
@@ -40,8 +104,8 @@ class HomePage extends StatelessWidget {
               'assets/icons/dots.svg',
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
